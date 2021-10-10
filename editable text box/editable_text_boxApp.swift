bb4cc74cdffef0961626173e58c9bb0f3f1b69fa
Franco Rod <81878824+Francorod@users.noreply.github.com>
2021-10-09 21:39:37 -0400

    Initial Commit

---
+++ editable text box/editable_text_boxApp.swift
@@ -0,0 +1,17 @@
+//
+//  editable_text_boxApp.swift
+//  editable text box
+//
+//  Created by Franco Rodrigues on 10/9/21.
+//
+
+import SwiftUI
+
+@main
+struct editable_text_boxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
